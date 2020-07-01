436cd3ae3ed0a2e48cbe3109b05f73677c892006
Nikita <a2143319@yandex.ru>
2020-07-01 16:12:35 +0500

    Initial Commit

---
+++ lesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by Никита Дмитриев on 01.07.2020.
+//  Copyright © 2020 Никита Дмитриев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
